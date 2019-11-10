9ef6b3f92c50817b7ca4835de40b1ad7195e94c8
Bob Farrell <robertanthonyfarrell@gmail.com>
2019-11-10 13:31:33 +0000

    Remove cruft

--- charts/.build/skep/docker/network.py
+++
@@ -1,7 +0,0 @@
-class Network:
-    def __init__(self, network):
-        self.id = network.id
-        self.name = network.name
-
-    def serializable(self):
-        return { 'id': self.id, 'name': self.name }
